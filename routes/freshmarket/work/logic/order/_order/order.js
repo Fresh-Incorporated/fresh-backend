91c3becdac0ad2e1498cabe37968110afdd448ba
zaralX <61970128+zaralX@users.noreply.github.com>
2025-01-08 13:01:26 +0800

    basic order accept and finish (logic)

---
+++ routes/freshmarket/work/logic/order/_order/order.js
@@ -0,0 +1,95 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+    fastify.addHook('onRequest', async (request, reply) => {
+        const User = fastify.sequelize.model('User');
+        const Product = fastify.sequelize.model('Product');
+        const Order = fastify.sequelize.model('Order');
+        const Location = fastify.sequelize.model('Location');
+        const LocationCell = fastify.sequelize.model('LocationCell');
+
+        try {
+            const accessToken = request.cookies.access_token;
+            if (!accessToken) {
+                return reply.status(401).send({ error: 'Missing access token' });
+            }
+
+            request.user = fastify.jwt.verify(accessToken);
+
+            request.user = await User.findOne({
+                where: {
+                    id: request.user.id
+                },
+                attributes: { exclude: ['updatedAt'] },
+            });
+
+            if (!request.user) {
+                return reply.status(400).send({
+                    message: "Пользователь не найден."
+                });
+            }
+
+            if (request.user.fm_worker < 2) {
+                return reply.status(403).send({
+                    message: "Недостаточно прав."
+                });
+            }
+
+            const order = await Order.findOne({
+                where: { id: request.params.order },
+            });
+
+            if (!order) {
+                return reply.status(400).send({
+                    message: "Товар не найден."
+                });
+            }
+
+            request.order = order
+        } catch (err) {
+            reply.status(401).send({ error: 'Unauthorized' });
+        }
+    });
+
+    fastify.post('/collect', async function (request, reply) {
+        const OrderHistory = fastify.sequelize.model('OrderHistory');
+
+        if (request.order.status !== 0) {
+            return reply.status(400).send({ message: "Задача недоступна (Возможно уже выполняется другим работником)"})
+        }
+
+        await request.order.update({
+            status: 1,
+            currentWorkerId: request.user.id,
+        })
+
+        await OrderHistory.create({
+            action_type: "collect_picked",
+            userId: request.user.id, // Тот кто взялся за сбор заказа
+            orderId: request.order.id,
+        })
+
+        return reply.status(200).send({product: request.product, message: "Задача на сбор заказа принята!"});
+    });
+
+    fastify.post('/collect/end', async function (request, reply) {
+        const OrderHistory = fastify.sequelize.model('OrderHistory');
+
+        if (request.order.status !== 1) {
+            return reply.status(400).send({ message: "Задача недоступна (Возможно уже завершена)"})
+        }
+
+        await request.order.update({
+            status: 2,
+            currentWorkerId: null,
+        })
+
+        await OrderHistory.create({
+            action_type: "collect_finished",
+            userId: request.user.id, // Тот кто взялся за сбор заказа
+            orderId: request.order.id,
+        })
+
+        return reply.status(200).send({product: request.product, message: "Задача на сбор заказа завершена!"});
+    });
+};
