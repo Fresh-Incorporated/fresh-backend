1d75883df559baf3c55e397cb4b0fb1c607746f4
zaralX <61970128+zaralX@users.noreply.github.com>
2025-04-11 20:34:59 +0800

    deposits under work

--- routes/bank/payments.js
+++ routes/bank/payments.js
@@ -15,8 +15,13 @@ module.exports = async function (fastify, opts) {
 
         const {data, amount} = request.body;
 
+        console.log(data)
+        console.log(amount)
+
         const type = data.split('_')[0]; // For updates [deposit]
         const id = data.split('_')[1];
+        console.log(type)
+        console.log(id)
 
         await User.increment({balance: amount}, {
             where: {
