c6a5a218062e9e14b5afb4da06ed00b9082a7696
zaralX <61970128+zaralX@users.noreply.github.com>
2025-04-11 20:50:40 +0800

    deposit payments completed

--- routes/bank/payments.js
+++ routes/bank/payments.js
@@ -7,8 +7,6 @@ module.exports = async function (fastify, opts) {
         const User = fastify.sequelize.model('User');
 
         const spwApi = new SPWorlds({ id: process.env.SPW_ID, token: process.env.SPW_TOKEN })
-        console.log(request.body)
-        console.log(request.headers)
         const isValid = spwApi.validateHash(request.body, request.headers['x-body-hash'])
 
         if (!isValid) {
@@ -17,20 +15,13 @@ module.exports = async function (fastify, opts) {
 
         const {data, amount} = request.body;
 
-        console.log(data)
-        console.log(amount)
-
         const type = data.split('_')[0]; // For updates [deposit]
         const id = data.split('_')[1];
-        console.log(type)
-        console.log(id)
 
         await User.increment({balance: amount}, {
             where: {
                 id: id
             }
         });
-
-        console.log("OK!")
     })
 }
