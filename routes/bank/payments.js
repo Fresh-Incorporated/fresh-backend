568d283babd148529e0106ac0569f8f900b0893d
zaralX <61970128+zaralX@users.noreply.github.com>
2025-04-11 20:24:07 +0800

    deposits under work

--- routes/bank/payments.js
+++ routes/bank/payments.js
@@ -1,7 +1,26 @@
 'use strict'
 
+const {SPWorlds} = require("spworlds");
+
 module.exports = async function (fastify, opts) {
     fastify.post('/spworlds/payment', async (request, reply) => {
-        console.log(request.body)
+        const User = fastify.sequelize.model('User');
+
+        const spwApi = new SPWorlds({ id: process.env.SPW_ID, token: process.env.SPW_TOKEN })
+        const isValid = spwApi.validateHash(request.body, request.headers['X-Body-Hash'])
+
+        if (!isValid) {
+            return reply.status(400).send({ message: 'Ошибка проверки цифровой подписи.' });
+        }
+
+        const {data, amount} = request.body;
+
+        await User.increment({balance: amount}, {
+            where: {
+                id: data.id
+            }
+        });
+
+        console.log("OK!")
     })
 }
