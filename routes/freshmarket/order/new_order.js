9d2fed70f046de373e297b44f2451f117418c845
zaralX <61970128+zaralX@users.noreply.github.com>
2025-04-17 18:10:14 +0800

    18 | deposit on buy cart

--- routes/freshmarket/order/new_order.js
+++ routes/freshmarket/order/new_order.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const {Op} = require("sequelize");
+const {SPWorlds} = require("spworlds");
 module.exports = async function (fastify, opts) {
     fastify.addHook('onRequest', async (request, reply) => {
         const User = fastify.sequelize.model('User');
@@ -29,7 +30,14 @@ module.exports = async function (fastify, opts) {
         }
     })
 
-    fastify.post('/new/instant', async function (request, reply) {
+    fastify.post('/new/instant', {
+        config: {
+            rateLimit: {
+                timeWindow: '5 minute',
+                max: 5
+            }
+        }
+    }, async function (request, reply) {
         const Product = fastify.sequelize.model('Product');
         const Shop = fastify.sequelize.model('Shop');
         const Order = fastify.sequelize.model('Order');
@@ -37,8 +45,8 @@ module.exports = async function (fastify, opts) {
         const ShopHistory = fastify.sequelize.model('ShopHistory');
         const Location = fastify.sequelize.model('Location');
 
-        const { type, branch } = request.body;
-        const { balance } = request.user;
+        const {type, branch} = request.body;
+        const {balance} = request.user;
 
         const products = request.body.products.map((product) => {
             product.count = parseInt(product?.count);
@@ -46,11 +54,11 @@ module.exports = async function (fastify, opts) {
         })
 
         if (type !== "branch") {
-            return reply.status(400).send({ message: "Сейчас доступна доставка только в филиалы!" });
+            return reply.status(400).send({message: "Сейчас доступна доставка только в филиалы!"});
         }
 
         if (products.length < 1) {
-            return reply.status(400).send({ message: "Корзина пуста!" });
+            return reply.status(400).send({message: "Корзина пуста!"});
         }
 
         const location = await Location.findOne({
@@ -63,11 +71,11 @@ module.exports = async function (fastify, opts) {
         })
 
         if (location == null) {
-            return reply.status(400).send({ message: "Доставка в выбранный филиал недоступна!" });
+            return reply.status(400).send({message: "Доставка в выбранный филиал недоступна!"});
         }
         const productIds = products.map(product => product.id);
         const productRows = await Product.findAll({
-            where: { id: productIds, verify_status: 1 },
+            where: {id: productIds, verify_status: 1},
             include: {
                 model: Shop,
                 as: "shop"
@@ -75,7 +83,7 @@ module.exports = async function (fastify, opts) {
         });
 
         if (productRows.length !== products.length) {
-            return reply.status(400).send({ message: "Некоторые товары не найдены." });
+            return reply.status(400).send({message: "Некоторые товары не найдены."});
         }
 
         let totalPrice = 0;
@@ -85,22 +93,55 @@ module.exports = async function (fastify, opts) {
             const productRow = productRows.find(row => row.id === product.id);
 
             if (!productRow) {
-                return reply.status(400).send({ message: `Товар с ID ${product.id} не найден.` });
+                return reply.status(400).send({message: `Товар с ID ${product.id} не найден.`});
             }
 
             if (product.count > productRow.count) {
-                return reply.status(400).send({ message: `Товара "${productRow.name}" недостаточно на складе.` });
+                return reply.status(400).send({message: `Товара "${productRow.name}" недостаточно на складе.`});
             }
 
             if (product.count < 1) {
-                return reply.status(400).send({ message: `Ты как 0 товара заказал гений?` });
+                return reply.status(400).send({message: `Ты как 0 товара заказал гений?`});
             }
 
             totalPrice += productRow.price * product.count;
         }
 
         if (balance < totalPrice) {
-            return reply.status(400).send({ message: "Недостаточно средств, пополните баланс." });
+            if (totalPrice < 1728) {
+                const spwApi = new SPWorlds({id: process.env.SPW_ID, token: process.env.SPW_TOKEN})
+                const pong = await spwApi.ping()
+
+                if (!pong) {
+                    return reply.status(500).send({message: 'SPWorlds API не доступен. Попробуйте позже.'});
+                }
+                const items = []
+                for (const product of products) {
+                    const productRow = productRows.find(row => row.id === product.id);
+                    const item = {
+                        name: productRow.name.length > 32 ? productRow.name.slice(0, 29) + '...' : productRow.name,
+                        count: product.count,
+                        price: productRow.price
+                    };
+
+                    if (productRow.description.length >= 3) {
+                        item.comment = productRow.description.length > 64
+                            ? productRow.description.slice(0, 61) + '...'
+                            : productRow.description;
+                    }
+
+                    items.push(item);
+                }
+                const payment = await spwApi.initPayment({
+                    items,
+                    redirectUrl: process.env.FRONTEND_URL + "/bank/payment/spworlds/completed",
+                    webhookUrl: process.env.BACKEND_URL + "/bank/spworlds/payment",
+                    data: 'deposit_' + request.user.id
+                })
+                return reply.status(400).send({message: "Недостаточно средств, пополните баланс.", url: payment.url});
+            } else {
+                return reply.status(400).send({message: "Недостаточно средств, пополните баланс."});
+            }
         }
 
         const transaction = await fastify.sequelize.transaction();
@@ -109,7 +150,7 @@ module.exports = async function (fastify, opts) {
             // Списываем средства с пользователя и обновляем количество продуктов
             for (const product of products) {
                 const productRow = productRows.find(row => row.id === product.id);
-                await Shop.increment({ balance: productRow.price * product.count * 0.9}, {
+                await Shop.increment({balance: productRow.price * product.count * 0.9}, {
                     where: {
                         id: productRow.shopId,
                     },
@@ -128,10 +169,10 @@ module.exports = async function (fastify, opts) {
                 }, {
                     transaction
                 })
-                await productRow.decrement({ count: product.count }, { transaction });
+                await productRow.decrement({count: product.count}, {transaction});
             }
 
-            await request.user.decrement({ balance: totalPrice }, { transaction });
+            await request.user.decrement({balance: totalPrice}, {transaction});
 
             const order = await Order.create({
                 customerId: request.user.id,
@@ -139,28 +180,28 @@ module.exports = async function (fastify, opts) {
                 price: totalPrice,
                 paid: true,
                 branchId: branch,
-                data: { products: products.map(({ id, count }) => ({ id, count })) },
-            }, { transaction });
+                data: {products: products.map(({id, count}) => ({id, count}))},
+            }, {transaction});
 
             await OrderHistory.create({
                 action_type: "created",
                 orderId: order.id,
                 userId: request.user.id
-            }, { transaction })
+            }, {transaction})
 
             await OrderHistory.create({
                 action_type: "paid",
                 orderId: order.id,
                 userId: request.user.id
-            }, { transaction })
+            }, {transaction})
 
             await transaction.commit();
-            return reply.status(200).send({ message: "Заказ оформлен." });
+            return reply.status(200).send({message: "Заказ оформлен."});
 
         } catch (error) {
             console.error(error);
             await transaction.rollback(); // Откатываем транзакцию при ошибке
-            return reply.status(500).send({ message: "Произошла ошибка при оформлении заказа.", error: error.message });
+            return reply.status(500).send({message: "Произошла ошибка при оформлении заказа.", error: error.message});
         }
     })
 }
